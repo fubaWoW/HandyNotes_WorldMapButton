c9cc5082f826ec952a82c88051c44239def45f91
fuba <q3fuba@gmail.com>
2020-09-15 22:29:32 +0200

    Moved the Button BACK to the "right" side again...
    Moved my Button to the "right" side of the map again!
    Compatibility now with:
    Mapster (Retail & Classic)
    Leatrix Maps (Retail & Classic)
    World Map Tracking Enhanced (Retail & Classic)

--- HandyNotes_WorldMapButton.lua
+++ HandyNotes_WorldMapButton.lua
@@ -1,29 +1,39 @@
+local AddOnName, AddOn = ...
 -- Handynotes Worldmap Button by fuba
 if not IsAddOnLoaded('HandyNotes') then return end
+
 local isClassicWow = select(4,GetBuildInfo()) < 20000
 
 local L = LibStub("AceLocale-3.0"):GetLocale("HandyNotes_WorldMapButton", false);
-local FOLDER_NAME = ...
-local Frame = CreateFrame("Frame");
 
-local iconDefault = [[Interface\AddOns\]] .. FOLDER_NAME .. [[\Buttons\Default]];
-local iconDisabled = [[Interface\AddOns\]] .. FOLDER_NAME .. [[\Buttons\Disabled]];
+local iconDefault = [[Interface\AddOns\]] .. AddOnName .. [[\Buttons\Default]];
+local iconDisabled = [[Interface\AddOns\]] .. AddOnName .. [[\Buttons\Disabled]];
 
---local btn = CreateFrame("Button", "HandyNotesWorldMapButton", WorldMapFrame.BorderFrame, "UIPanelButtonTemplate");
-local btn = CreateFrame("Button", "HandyNotesWorldMapButton", WorldMapFrame.ScrollContainer, "UIPanelButtonTemplate");
+local point, relativeTo, relativePoint, xOfs, yOfs
+
+local ButtonName = "HandyNotesWorldMapButton"
+local btn = _G[ButtonName]
 
 local function SetIconTexture()
-	if HandyNotes:IsEnabled() then
-		btn:SetNormalTexture(iconDefault);
+	local btn = _G[ButtonName]
+	if not btn then return end
+	if HandyNotes then
+		if HandyNotes:IsEnabled() then
+			btn:SetNormalTexture(iconDefault);
+		else
+			btn:SetNormalTexture(iconDisabled);
+		end
 	else
-		btn:SetNormalTexture(iconDisabled);
+		btn:Hide();
 	end
 end
 
 local function SetIconTooltip(IsRev)
+	local btn = _G[ButtonName]
+	if not btn then return end
 	if not WorldMapTooltip then return end
 	WorldMapTooltip:Hide();
-	WorldMapTooltip:SetOwner(btn, "ANCHOR_BOTTOMRIGHT");
+	WorldMapTooltip:SetOwner(btn, "ANCHOR_BOTTOMLEFT");
 	if HandyNotes:IsEnabled() then
 		WorldMapTooltip:AddLine(L["TEXT_TOOLTIP_HIDE_ICONS"], nil, nil, nil, nil, 1 );
 	else
@@ -31,14 +41,6 @@ local function SetIconTooltip(IsRev)
 	end
 	WorldMapTooltip:Show();
 end
-
-btn:ClearAllPoints();
-btn:SetPoint("TOPLEFT", WorldMapFrame.ScrollContainer, "TOPLEFT", 6, -3)
-btn:RegisterForClicks("AnyUp");
-btn:SetSize(24, 24);
-btn:SetText("");
-SetIconTexture();
-btn:Hide();
 
 local function btnOnEnter(self, motion)
 	SetIconTexture();
@@ -66,49 +68,83 @@ local function btnOnClick(self)
 	SetIconTooltip(false);
 end
 
+hooksecurefunc(HandyNotes, "OnEnable", function(self)
+	SetIconTexture()
+end)
+
+hooksecurefunc(HandyNotes, "OnDisable", function(self)
+	SetIconTexture()
+end)
+
+
+if isClassicWow then
+	btn = _G[ButtonName] or CreateFrame("Button", ButtonName, WorldMapFrame, "UIPanelButtonTemplate");
+else
+	btn = _G[ButtonName] or CreateFrame("Button", ButtonName, WorldMapFrame.ScrollContainer, "UIPanelButtonTemplate");
+end
+btn:RegisterForClicks("AnyUp");
+btn:SetText("");
 btn:SetScript("OnClick", btnOnClick);
 btn:SetScript("OnEnter", btnOnEnter);
 btn:SetScript("OnLeave", btnOnLeave);
 
-local function OnEvent(self, event, addon, ...)
-	if (event == "PLAYER_ENTERING_WORLD") then
-		if IsAddOnLoaded('HandyNotes') then
-			if btn then
-				SetIconTexture();
-				btn:Show();
-			end;
+WorldMapFrame:HookScript("OnShow", function(self)		
+	if isClassicWow then
+		btn = _G[ButtonName] or CreateFrame("Button", ButtonName, WorldMapFrame, "UIPanelButtonTemplate");
+		btn:ClearAllPoints();
+		btn:SetPoint("TOPRIGHT", WorldMapFrame, "TOPRIGHT", -16, -76)
+		btn:SetSize(24, 24);
+		btn:SetFrameLevel(5000);
+	else
+		local FilterButton = WorldMapFrame.overlayFrames[2]
+		btn = _G[ButtonName] or CreateFrame("Button", ButtonName, WorldMapFrame.ScrollContainer, "UIPanelButtonTemplate");
+		btn:ClearAllPoints();
+		if FilterButton and FilterButton:IsVisible() then
+			parent = FilterButton:GetParent()	
+			btn:SetPoint("RIGHT", FilterButton, "LEFT", -5, 0)
+			btn:SetSize(24, 24);
+			btn:SetFrameLevel(5000);
 		else
-			if btn then btn:Hide(); end
+			btn:SetPoint("TOPRIGHT", WorldMapFrame.ScrollContainer, "TOPRIGHT", -10, -7)
+			btn:SetSize(24, 24);
+			btn:SetFrameLevel(5000);
 		end
 	end
-
-	-- Move the MapsterOptionsButton by the Width of HandyNotesWorldMapButton to prevent overlap
-	if event == "ADDON_LOADED" then
-		--[[
-		if addon == "Mapster" then
-			if (MapsterOptionsButton) and (not MapsterOptionsButton.MovedByHandyNotesWorldMapButton) then
-				point, relativeTo, relativePoint, xOfs, yOfs = MapsterOptionsButton:GetPoint()
-				MapsterOptionsButton:ClearAllPoints();
-				MapsterOptionsButton:SetPoint(point, relativeTo, relativePoint, xOfs - btn:GetWidth() - 5, yOfs);
-				Frame:UnregisterEvent("ADDON_LOADED");
-				MapsterOptionsButton.MovedByHandyNotesWorldMapButton = true;
-			end
-		elseif addon == "Leatrix_Maps" then
-			if LeaMapsDB["NoMapBorder"] == "On" then
-				if btn then
-					if isClassicWow then
-						btn:ClearAllPoints();
-						-btn:SetPoint("RIGHT", WorldMapFrameCloseButton, "LEFT", 0, 0);
-						btn:SetFrameLevel(5000);
-					else
+	
+	if IsAddOnLoaded('Leatrix_Maps') then
+		if isClassicWow then
+			if LeaMapsDB and (LeaMapsDB["NoMapBorder"] == "On" and LeaMapsDB["UseDefaultMap"] == "Off") then
+				if IsAddOnLoaded('WorldMapTrackingEnhanced') then
+					local wmteb = _G["WorldMapTrackingEnhancedButton"];
+					if wmteb then
+						if WorldMapFrameCloseButton and WorldMapFrameCloseButton:IsVisible() then
+							wmteb:ClearAllPoints()
+							wmteb:SetPoint("RIGHT", WorldMapFrameCloseButton, "LEFT", -5, 0)
+							btn = _G[ButtonName] or CreateFrame("Button", ButtonName, WorldMapFrame, "UIPanelButtonTemplate");
+							btn:ClearAllPoints();
+							btn:SetPoint("RIGHT", WorldMapTrackingEnhancedButton, "LEFT", -5, 0);
+							btn:SetFrameLevel(5000);
+							btn:SetSize(24, 24);
+						end
 					end
+				else
+					btn = _G[ButtonName] or CreateFrame("Button", ButtonName, WorldMapFrame, "UIPanelButtonTemplate");
+					btn:ClearAllPoints();
+					btn:SetPoint("RIGHT", WorldMapFrameCloseButton, "LEFT", 0, 0);
+					btn:SetFrameLevel(5000);
+					btn:SetSize(18, 18);
+				end
+			else
+				if IsAddOnLoaded('WorldMapTrackingEnhanced') then
+					btn = _G[ButtonName] or CreateFrame("Button", ButtonName, WorldMapFrame, "UIPanelButtonTemplate");
+					btn:ClearAllPoints();
+					btn:SetPoint("RIGHT", WorldMapTrackingEnhancedButton, "LEFT", -5, 0);
+					btn:SetFrameLevel(5000);
+					btn:SetSize(24, 24);					
 				end
 			end
 		end
-		]]
 	end
-end
-
-Frame:RegisterEvent("PLAYER_ENTERING_WORLD")
-Frame:RegisterEvent("ADDON_LOADED")
-Frame:SetScript("OnEvent", OnEvent)
+	btn:Show();	
+	SetIconTexture();
+end)
